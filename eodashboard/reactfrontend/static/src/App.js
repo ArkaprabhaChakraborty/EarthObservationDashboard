2e0f5cb548ec5650dde3402130cb2697032c45e1
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-05-12 03:19:19 +0530

    initial files added

---
+++ eodashboard/reactfrontend/static/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Dact from './dact.svg';
+
+const App = () => {
+return(
+        <div className="App">
+            <div className="Navbar">
+                Hello world
+                Welcome to eodashboard
+            </div>
+        </div>
+        );
+}
+export default App;
