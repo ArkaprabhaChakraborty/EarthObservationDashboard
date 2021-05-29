ebc224eaaf438ba39a6efa92ac02abc62aeebbb0
Aniruddha Basak <codewithaniruddha@gmail.com>
2021-05-29 13:46:58 +0530

    Add method to fetch all data from day one by country

--- eodashboard/reactfrontend/api.py
+++ eodashboard/reactfrontend/api.py
@@ -1,3 +1,5 @@
+import requests
+
 class Covid_19():
     """
     Covid 19 api method
@@ -5,7 +7,7 @@ class Covid_19():
     def get_countries(self):
         """
         Gets all the country with code
-        Access data by countries[i]["IS02"] # i is the index
+        Access data by context['countries'][i]["IS02"] # i is the index
         """
         url = "https://api.covid19api.com/countries"
         response = requests.get(url)
@@ -18,11 +20,12 @@ class Covid_19():
             context = {
                 "messege": "Bad requests."
             }
+        return context
 
     def get_summary(self):
         """
         Gets summary of new and total cases per country updated daily.
-        Access data by country_data[i]["TotalConfirmed"] # i is the index
+        Access data by context['country_data'][i]["TotalConfirmed"] # i is the index
         global_data["TotalRecovered"]
         """
         url = "https://api.covid19api.com/summary"
@@ -34,6 +37,25 @@ class Covid_19():
             context = {
                 "global_data": global_data,
                 "country_data": country_data,
+            }
+        else:
+            context = {
+                "messege": "Bad requests."
+            }
+        return context
+
+    def get_data_from_day_one_by_country(self, country):
+        """
+        Returns all cases by case type for a country from the first recorded case.
+        country should be from country/ or summary/ endpoint.
+        Access data by context['data_from_day_one_by_country'][i]["Recovered"] # i is the index
+        """
+        url = "https://api.covid19api.com/total/dayone/country/{country}".format(country=country)
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            context = {
+                "data_from_day_one_by_country": data,
             }
         else:
             context = {
