31ac09703a35aa88836aa95ef4d56448d1615613
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-06-29 01:23:30 +0530

    router added

--- eodashboard/reactfrontend/static/src/App.js
+++ eodashboard/reactfrontend/static/src/App.js
@@ -3,16 +3,35 @@ import './App.css';
 import Navbar from './Navbar';
 import Covid from './Covid';
 import Airquality from './airqualitymap';
+import {Switch, Route} from 'react-router-dom';
+import {BrowserRouter} from "react-router-dom";
+
 
 const App = () => {
 return(
+    <BrowserRouter>
         <div className="App">
             <Navbar/>
             <div className="App_page">
-                <Covid/>
-                <Airquality/>
+                
+                <Switch>
+                    <Route path="/" exact>
+                        <Covid/>
+                        <Airquality/>
+                    </Route>
+                    <Route path="/agro" exact>
+                        <div>AGROSTUFFS</div>
+                    </Route>
+                    <Route path="/about">
+                        <div>ABOUT US</div>
+                    </Route>
+                    <Route path="/atm">
+                        <div>SOMETHING</div>
+                    </Route>
+                </Switch>
             </div>
         </div>
+        </BrowserRouter>
         );
 }
 export default App;
